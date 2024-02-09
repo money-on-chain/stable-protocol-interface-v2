e1ade6f906a99a593b8c907f88c818bb6c4e91e5
Sebas <sheva3232@gmail.com>
2024-02-09 13:49:34 -0500

    feat(color theme) : Adding QR workable function and onclick on address on user modal Task ROC-715

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
+import QRCode from "react-qr-code";
 
 import { useProjectTranslation } from '../../helpers/translations';
 import { AuthenticateContext } from '../../context/Auth';
@@ -11,7 +12,13 @@ export default function AccountDialog(props) {
 
     const [t, i18n, ns] = useProjectTranslation();
     const auth = useContext(AuthenticateContext);
-
+    const [qrValue, setQrValue] = useState(null);
+    
+    useEffect(() => {
+        const url = process.env.REACT_APP_ENVIRONMENT_EXPLORER_URL + '/address/' + auth.accountData.Wallet;
+        setQrValue(url);
+    }, [auth, auth.accountData.Wallet]);
+    
     const onClose = () => {
         onCloseModal();
     };
@@ -20,16 +27,27 @@ export default function AccountDialog(props) {
         onCloseModal();
         auth.disconnect();
     };
-
     return (
         <div className="AccountDialog">
-
             <div className="qr">
-
+                <QRCode
+                    size={256}
+                    style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
+                    value={qrValue ?? ''}
+                    viewBox={`0 0 256 256`}
+                />
             </div>
 
-            <div className="address">
-                <div className="caption">Address</div> <div className="truncate-address">{truncatedAddress}</div>
+            <div
+                className="address"
+                style={{ cursor: qrValue ? 'pointer' : 'default' }}
+                onClick={() => {
+                    if (!qrValue) return;
+                    window.open(qrValue, '_blank', 'noopener,noreferrer');
+                }}
+            >
+                <div className="caption">Address</div>{' '}
+                <div className="truncate-address">{truncatedAddress}</div>
             </div>
 
             <div className="actions">
@@ -48,7 +66,6 @@ export default function AccountDialog(props) {
                     Close
                 </button>
             </div>
-
         </div>
     );
 }
