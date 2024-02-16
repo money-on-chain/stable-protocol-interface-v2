f06e3834df2a7510ad5671771c18ea5740a186c9
Sebas <sheva3232@gmail.com>
2024-02-16 17:12:51 -0500

    deleting unused code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,12 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import { useContext } from 'react';
-import { Row, Alert } from 'antd';
+import React, { Fragment } from 'react';
 
-import { AuthenticateContext } from '../../context/Auth';
 import ListOperations from '../../components/Tables/ListOperations';
-import { useProjectTranslation } from '../../helpers/translations';
 
 import '../../assets/css/pages.scss';
 import StakingRewards from '../../components/Dashboards/StakingRewards';
 import Portfolio from '../../components/Dashboards/Portfolio';
 
 function Home(props) {
-    const [t, i18n, ns] = useProjectTranslation();
-    const auth = useContext(AuthenticateContext);
     return (
         <Fragment>
 
