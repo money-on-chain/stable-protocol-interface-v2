a4c5f45b1de9ae25ee62fa9f713b336f8c9aec2d
Sebas <sheva3232@gmail.com>
2024-03-19 21:29:32 +0000

    fix: error message on positive TXs

--- src/helpers/errorHandler.js
+++ src/helpers/errorHandler.js
@@ -1,7 +1,4 @@
 export const GetErrorMessage = (error) => {
-  console.log('error to be shown in errorhandler ....' , error);
-  console.log(error === 'null');
-  console.log(error === null);
   switch (error) {
     case 'qAC below minimum required':
       return 'Redeem TP or TC RIF price up';
