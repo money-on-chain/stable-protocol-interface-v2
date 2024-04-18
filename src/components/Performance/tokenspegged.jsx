4e9738dc452c7df73bac23a556ffded44c8e94cf
Sebas <sheva3232@gmail.com>
2024-04-18 10:15:49 -0500

    fixing issue on operatio table title

--- src/components/Performance/tokenspegged.jsx
+++ src/components/Performance/tokenspegged.jsx
@@ -177,7 +177,11 @@ const ProvideColumnsTP = [
         <div className="card-tps">
 
             <div className="title">
-                <h1>{`${t('performance.pegged.cardTitle')} ${settings.tokens.TP[0].name}`}</h1>
+                <h1>{
+                    i18n.language === 'en'
+                        ? `${settings.tokens.TP[0].name} ${t('performance.pegged.cardTitle')}`
+                        : `${t('performance.pegged.cardTitle')} ${settings.tokens.TP[0].name}`
+                }</h1>
             </div>
 
             <Table
