ee485a0fd4125c405aede0cc98638270690b06e2
mulonemartin <mulone.martin@gmail.com>
2024-07-19 16:12:09 -0300

    fix: add vesting and select

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -1,16 +1,57 @@
 import React, { useContext, useState, useEffect } from 'react';
 import QRCode from 'react-qr-code';
-import { notification, Switch, Select, Button } from 'antd';
+import { notification, Switch, Select, Button, Input } from 'antd';
 
 import { useProjectTranslation } from '../../helpers/translations';
 import { AuthenticateContext } from '../../context/Auth';
+import BigNumber from 'bignumber.js';
+import VestingMachine from '../../contracts/omoc/VestingMachine.json';
+
+
+const { Option } = Select;
+
+
+function removeAllItem(arr, value) {
+    let i = 0;
+    while (i < arr.length) {
+        if (arr[i] === value) {
+            arr.splice(i, 1);
+        } else {
+            ++i;
+        }
+    }
+    return arr;
+}
+
+
 export default function AccountDialog(props) {
-    const { onCloseModal, truncatedAddress } = props;
+    const { onCloseModal, truncatedAddress, vestingOn } = props;
 
     const [t, i18n, ns] = useProjectTranslation();
     const auth = useContext(AuthenticateContext);
     const [qrValue, setQrValue] = useState(null);
-    const [showAdditional, setShowAdditional] = useState(false);
+    const [showVesting, setShowVesting] = useState(vestingOn);
+    const [actionVesting, setActionVesting] = useState('select');
+    const [addVestingAddress, setAddVestingAddress] = useState('');
+    const [addVestingAddressError, setAddVestingAddressError] = useState(false);
+    const [addVestingAddressErrorText, setAddVestingAddressErrorText] = useState('');
+
+    const loadVestingAddressesFromLocalStorage = () => {
+        const storageVestingAddresses = localStorage.getItem("vesting-addresses");
+        let vestingAddresses = []
+        if(storageVestingAddresses !== null){
+            vestingAddresses = JSON.parse(storageVestingAddresses)
+        }
+        return vestingAddresses
+    };
+
+    const defaultVestingAddresses = loadVestingAddressesFromLocalStorage();
+    let defaultVestingAddress = null
+    // Select the first one from the list of vesting
+    if (defaultVestingAddresses) defaultVestingAddress = defaultVestingAddresses[0]
+
+    const [vestingAddresses, setVestingAddresses] = useState(defaultVestingAddresses);
+    const [vestingAddressDefault, setVestingAddressDefault] = useState(defaultVestingAddress);
 
     useEffect(() => {
         const url = process.env.REACT_APP_ENVIRONMENT_EXPLORER_URL + '/address/' + auth.accountData.Wallet;
@@ -25,6 +66,7 @@ export default function AccountDialog(props) {
         onCloseModal();
         auth.disconnect();
     };
+
     const onCopy = (e) => {
         e.stopPropagation();
         navigator.clipboard.writeText(address);
@@ -34,6 +76,124 @@ export default function AccountDialog(props) {
             placement: 'bottomRight'
         });
     };
+
+    const onChangeInputVestingAddress = (e) => {
+        setAddVestingAddress(e.target.value);
+        onValidateVestingAddressClear();
+    };
+
+    const onValidateVestingAddressClear = () => {
+        setAddVestingAddressErrorText('');
+        setAddVestingAddressError(false);
+    };
+
+    const onValidateVestingAddress = () => {
+
+        // 1. Input address valid
+        if (addVestingAddress === '') {
+            setAddVestingAddressErrorText('Vesting address can not be empty');
+            setAddVestingAddressError(true);
+            return false;
+        } else if (addVestingAddress.length < 42 || addVestingAddress.length > 42) {
+            setAddVestingAddressErrorText('Not valid input vesting address');
+            setAddVestingAddressError(true);
+            return false
+        }
+
+        // 2. Check if not in the list
+        /*const vestingLowerCase = vestingAddresses.map(function(value){return value.toLowerCase()});
+        if (vestingLowerCase.includes(addVestingAddress)) {
+            setAddVestingAddressErrorText('Address is already added!');
+            setAddVestingAddressError(true);
+            return false
+        }*/
+
+        try {
+            const vestingMachine = new auth.web3.eth.Contract(
+                VestingMachine.abi,
+                addVestingAddress
+            );
+            const holder = vestingMachine.methods.getHolder().call()
+            console.log("Holder: ", holder);
+
+            return true
+        } catch (error) {
+            console.log(`Invalid Vesting address: ${error}`);
+            setAddVestingAddressErrorText('Seems that address is not valid vesting');
+            setAddVestingAddressError(true);
+            return false
+        }
+    };
+
+    const saveVestingAddressesToLocalStorage = (vAddresses) => {
+
+        // Store vesting addresses
+        const sVestingAddresses =
+            JSON.stringify(vAddresses)
+
+        // save to storage addresses
+        localStorage.setItem(
+            "vesting-addresses",
+            sVestingAddresses
+        )
+
+    }
+
+    const saveDefaultVestingToLocalStorage = (vAddress) => {
+
+        // Save as the default vesting also
+        localStorage.setItem(
+            "default-vesting-address",
+            vAddress
+        )
+
+    }
+
+    const onAddVesting = (e) => {
+        e.stopPropagation();
+        const isValidVesting = onValidateVestingAddress();
+        if (isValidVesting) {
+            //add on storage
+
+            // get vesting addresses
+            const vestingAddresses = loadVestingAddressesFromLocalStorage()
+
+            //Add the new one to the list
+            vestingAddresses.push(addVestingAddress)
+
+            // Store vesting addresses
+            saveVestingAddressesToLocalStorage(vestingAddresses)
+            saveDefaultVestingToLocalStorage(addVestingAddress)
+
+            setVestingAddresses(vestingAddresses)
+            setVestingAddressDefault(addVestingAddress)
+
+            // Close add panel
+            setActionVesting('select');
+        }
+
+    };
+
+    const onUnloadVM = (e) => {
+        e.stopPropagation();
+        const removeItems = removeAllItem(vestingAddresses, vestingAddressDefault);
+        saveVestingAddressesToLocalStorage(removeItems);
+        setVestingAddressDefault(null);
+        setVestingAddresses(removeItems);
+        console.log("DEBUG>>>")
+        console.log(removeItems)
+    };
+
+    const onShowAddVesting = (e) => {
+        e.stopPropagation();
+        setActionVesting('add');
+    };
+
+    const onCloseAddVesting = (e) => {
+        e.stopPropagation();
+        setActionVesting('select');
+    };
+
     return (
         <div className="AccountDialog">
             <div className="title">{t('wallet.modalTitle')}</div>
@@ -51,9 +211,6 @@ export default function AccountDialog(props) {
                             <a onClick={onCopy}>
                                 <i className="icon-copy"></i>
                             </a>
-                            <a onClick={onCopy}>
-                                <i className="icon-copy"></i>
-                            </a>
                         </div>
                     </div>
                 </div>
@@ -63,30 +220,57 @@ export default function AccountDialog(props) {
             </div>
 
             <div className="switch">
-                <Switch checked={showAdditional} onChange={(checked) => setShowAdditional(checked)} />
+                <Switch checked={showVesting} onChange={(checked) => setShowVesting(checked)} />
                 <p>{t('wallet.useVesting')}</p>
             </div>
 
-            {showAdditional && (
+            {showVesting && actionVesting === 'select' && (
                 <div className="additional-content">
                     <div className="dropdown-label">{t('wallet.inputLabel')}</div>
                     <div className="dropdown">
-                        <Select defaultValue="option1" style={{ width: 120 }}>
-                            <Option value="option1">Option 1</Option>
-                            <Option value="option2">Option 2</Option>
+                        <Select value={vestingAddressDefault} style={{ width: 120 }}>
+                            {vestingAddresses.map((possibleOption) => (
+                                <Option
+                                    key={possibleOption}
+                                    value={possibleOption}
+                                >
+                                    {possibleOption}
+                                </Option>
+                            ))}
                         </Select>
                         <Button onClick={onCopy}>
                             <i className="icon-copy"></i>
                         </Button>
                     </div>
                     <div className="buttons">
-                        <button>{t('wallet.loadVM')}</button>
-                        <button>{t('wallet.unloadVM')}</button>
-                        <button>{t('wallet.useClaimCode')}</button>
+                        <button onClick={onShowAddVesting}>{t('wallet.loadVM')}</button>
+                        <button onClick={onUnloadVM}>{t('wallet.unloadVM')}</button>
                     </div>
                     <div className="additional-text">{t('wallet.disclaimer')}</div>
                 </div>
             )}
+
+            {showVesting && actionVesting === 'add' && (
+                <div className="additional-content">
+                    <div className="dropdown-label">Add Vesting</div>
+                    <div className="dropdown">
+                        <Input type="text" placeholder="vesting address" className="input-address" onChange={onChangeInputVestingAddress} />
+                        {addVestingAddressError && addVestingAddressErrorText !== '' && (<div className={'input-error'}>{addVestingAddressErrorText}</div>)}
+                    </div>
+                    <div className="actions">
+                        <button type="secondary" className="secondary-button btn-clear" onClick={onCloseAddVesting}>
+                            Cancel
+                        </button>
+                        <button type="primary" className="primary-button btn-confirm" onClick={onAddVesting}>
+                            Add
+                        </button>
+                    </div>
+                    <div className="additional-text">{t('wallet.disclaimer')}</div>
+                </div>
+            )}
+
+
+
             <div className="actions">
                 <button type="secondary" className="secondary-button btn-clear" onClick={onDisconnect}>
                     {t('wallet.cta.disconnect')}
