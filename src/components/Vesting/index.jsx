a59754debcf1de4e8f5aa3c39c483e1e06ac62af
mulonemartin <mulone.martin@gmail.com>
2024-09-20 14:40:31 -0300

    fix: Auto load new vesting on event

--- src/components/Vesting/index.jsx
+++ src/components/Vesting/index.jsx
@@ -323,7 +323,20 @@ export default function Vesting(props) {
                     if (events.name === 'VestingCreated') {
                         events.events.forEach(function (field) {
                             if (field.name === 'vesting') {
-                                setNewVestingAddress(field.value.toLowerCase());
+                                const vNewAddress = field.value.toLowerCase();
+                                setNewVestingAddress(vNewAddress);
+
+                                // set go to step Nº 4
+                                setStatus('STEP_4');
+
+                                // Close the modal
+                                setIsOperationModalVisible(false);
+
+                                // Add vesting address to storage
+                                addVesting(vNewAddress).then((results) => {
+                                }).catch((error) => {
+                                    console.log(error);
+                                });
                             }
                         });
                     }
@@ -352,10 +365,6 @@ export default function Vesting(props) {
             setOperationStatus('success');
             const filteredEvents = auth.interfaceDecodeEvents(receipt);
             onVestingCreated(filteredEvents);
-            // set go to step Nº 4
-            setStatus('STEP_4');
-            // Close the modal
-            setIsOperationModalVisible(false);
         };
         const onError = (error) => {
             console.log('Transaction create VM error!...:', error);
@@ -416,10 +425,6 @@ export default function Vesting(props) {
 
             return true
         }
-    }
-
-    const onClickAddVesting = async() => {
-        await addVesting(newVestingAddress);
     }
 
     return (
@@ -642,9 +647,7 @@ export default function Vesting(props) {
                                         'vesting.vestingOnboarding.page2.ctaSecondary'
                                     )}
                                 </button>
-                                <button className='button' onClick={onClickAddVesting} disabled={newVestingAddress===''}>
-                                    Finish & Add vesting
-                                </button>
+
                             </div>
 
                             <div className='pagination'>
