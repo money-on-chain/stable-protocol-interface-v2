364f2ee8beceb14aef170b0b16852f698fc9530e
mulonemartin <mulone.martin@gmail.com>
2024-10-11 00:46:13 -0300

    fix: staking rework

--- src/components/Staking/index.jsx
+++ src/components/Staking/index.jsx
@@ -6,6 +6,7 @@ import Stake from './Stake';
 import PieChartComponent from './PieChart';
 import PerformanceChart from './performanceChart';
 import Withdraw from './Withdraw';
+import DashBoard from './DashBoard';
 import { AuthenticateContext } from '../../context/Auth';
 import Web3 from 'web3';
 
@@ -58,7 +59,7 @@ export default function Staking(props) {
         }
 
         const lockedAmount = new BigNumber(
-            Web3.utils.fromWei(vUsing.getLockingInfo.amount,'ether')
+            Web3.utils.fromWei(vUsing.getLockingInfo.amount, 'ether')
         );
         const lockedUntilTimestamp = new BigNumber(
             vUsing.getLockingInfo.untilTimestamp
@@ -99,7 +100,7 @@ export default function Staking(props) {
                 );
             }
         });
-        const pendingWithdrawalsSort = pendingWithdrawalsFormatted.sort(function (a, b) {
+        const pendingWithdrawalsSort = pendingWithdrawalsFormatted.sort(function(a, b) {
             return b.id.toString() - a.id.toString();
         });
 
@@ -111,71 +112,79 @@ export default function Staking(props) {
     };
 
     return (
-        <div className="cards-container sectionStaking">
-            <Fragment>
-                <div className="section row-section">
-                    {' '}
-                    <div className="firstCardsGroup">
-                        <div id="stakingCard" className="layout-card">
-                            <div className="layout-card-title">
-                                <h1>{t('staking.cardTitle')}</h1>
+        <div>
+            <div className={'section-layout'}>
+                <DashBoard userInfoStaking={userInfoStaking} />
+            </div>
+
+            <div className={'section-layout'}>
+                <div className="cards-container sectionStaking">
+                    <Fragment>
+                        <div className="section row-section">
+                            {' '}
+                            <div className="firstCardsGroup">
+                                <div id="stakingCard" className="layout-card">
+                                    <div className="layout-card-title">
+                                        <h1>{t('staking.cardTitle')}</h1>
+                                    </div>
+                                    <div className="tabs">
+                                        <button
+                                            onClick={() => setActiveTab('tab1')}
+                                            className={`tab-button ${activeTab === 'tab1' ? 'active' : ''}`}
+                                        >
+                                            {t('staking.staking.tabStake')}
+                                        </button>
+                                        <button
+                                            onClick={() => setActiveTab('tab2')}
+                                            className={`tab-button ${activeTab === 'tab2' ? 'active' : ''}`}
+                                        >
+                                            {t('staking.staking.tabUnstake')}
+                                        </button>
+                                    </div>
+                                    <div className="tab-divider"></div>
+                                    {/* Tab Content */}
+                                    <div className="tab-content">
+                                        <Stake
+                                            activeTab={activeTab}
+                                            userInfoStaking={userInfoStaking}
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div
+                                        id="distributionCard"
+                                        className="layout-card staking-distribution-card"
+                                    >
+                                        <div className="layout-card-title">
+                                            <h1>
+                                                {t('staking.distribution.title')}
+                                            </h1>
+                                        </div>
+                                        <div className="tab-content">
+                                            <PieChartComponent
+                                                userInfoStaking={userInfoStaking}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="performanceCard" className="layout-card">
+                                    <div className="layout-card-title">
+                                        <h1>{t('staking.performance.title')}</h1>
+                                    </div>
+                                    <div className="tab-content">
+                                        <PerformanceChart />
+                                    </div>
+                                </div>
                             </div>
-                            <div className="tabs">
-                                <button
-                                    onClick={() => setActiveTab('tab1')}
-                                    className={`tab-button ${activeTab === 'tab1' ? 'active' : ''}`}
-                                >
-                                    {t('staking.staking.tabStake')}
-                                </button>
-                                <button
-                                    onClick={() => setActiveTab('tab2')}
-                                    className={`tab-button ${activeTab === 'tab2' ? 'active' : ''}`}
-                                >
-                                    {t('staking.staking.tabUnstake')}
-                                </button>
-                            </div>
-                            <div className="tab-divider"></div>
-                            {/* Tab Content */}
-                            <div className="tab-content">
-                                <Stake
-                                    activeTab={activeTab}
+                            <div>
+                                <Withdraw
                                     userInfoStaking={userInfoStaking}
                                 />
                             </div>
                         </div>
-                        <div>
-                            <div
-                                id="distributionCard"
-                                className="layout-card staking-distribution-card"
-                            >
-                                <div className="layout-card-title">
-                                    <h1>
-                                        {t('staking.distribution.title')}
-                                    </h1>
-                                </div>
-                                <div className="tab-content">
-                                    <PieChartComponent
-                                        userInfoStaking={userInfoStaking}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div id="performanceCard" className="layout-card">
-                            <div className="layout-card-title">
-                                <h1>{t('staking.performance.title')}</h1>
-                            </div>
-                            <div className="tab-content">
-                                <PerformanceChart />
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <Withdraw
-                            userInfoStaking={userInfoStaking}
-                        />
-                    </div>
+                    </Fragment>
                 </div>
-            </Fragment>
+            </div>
         </div>
-    );
-}
+        );
+        }
