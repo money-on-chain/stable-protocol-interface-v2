01f949caf219ef12a424e7422ace8ee2d0f9e6e7
followleohans <lehans@gmail.com>
2025-01-21 14:32:50 -0300

    CurrencySelector renamed to CurrencyDropDown. CurrencyPopUp implemented instead.

--- src/components/CurrencyPopUp/index.jsx
+++ src/components/CurrencyPopUp/index.jsx
@@ -1,11 +1,11 @@
-import React, { useContext, useState } from 'react';
-import { Modal, Button } from 'antd';
-import { getCurrenciesDetail } from '../../helpers/currencies';
-import { AuthenticateContext } from '../../context/Auth';
-import { useProjectTranslation } from '../../helpers/translations';
-import './Styles.scss';
+import React, { useContext, useState } from "react";
+import { Modal, Button } from "antd";
+import { getCurrenciesDetail } from "../../helpers/currencies";
+import { AuthenticateContext } from "../../context/Auth";
+import { useProjectTranslation } from "../../helpers/translations";
+import "./Styles.scss";
 
-export default function SelectCurrency(props) {
+export default function CurrencyPopUp(props) {
     const { value, onChange, currencyOptions, disabled, action, title } = props;
     const [t, i18n, ns] = useProjectTranslation();
     const [isModalVisible, setIsModalVisible] = useState(false);
@@ -15,7 +15,7 @@ export default function SelectCurrency(props) {
         value: currency.value,
         image: currency.image,
         label: t(`${action}.tokens.${currency.value}.label`, { ns: ns }),
-        abbreviation: t(`${action}.tokens.${currency.value}.abbr`, { ns: ns })
+        abbreviation: t(`${action}.tokens.${currency.value}.abbr`, { ns: ns }),
     }));
 
     // Get the currently selected currency
@@ -49,7 +49,7 @@ export default function SelectCurrency(props) {
     };
 
     return (
-        <div className={`SelectCurrency ${disabled ? 'disabled' : ''}`}>
+        <div className={`SelectCurrency ${disabled ? "disabled" : ""}`}>
             {/* Button or selected currency to open the modal */}
             <div className={`${action}-select-token`} onClick={openModal}>
                 {selectedCurrency ? (
@@ -72,7 +72,7 @@ export default function SelectCurrency(props) {
 
             {/* Ant Design Modal */}
             <Modal
-                title={title && title.trim() !== '' ? title : 'Select a Token'} // Custom title support
+                title={title && title.trim() !== "" ? title : "Select a Token"} // Custom title support
                 visible={isModalVisible}
                 onCancel={closeModal}
                 footer={null}
@@ -82,7 +82,7 @@ export default function SelectCurrency(props) {
                     {filteredOptions.map((currency) => (
                         <div
                             key={currency.value}
-                            className={`token-item ${currency.value === value ? 'is-selected' : ''}`}
+                            className={`token-item ${currency.value === value ? "is-selected" : ""}`}
                             onClick={() => handleSelect(currency.value)}
                         >
                             <div className="token__icon">{currency.image}</div>
