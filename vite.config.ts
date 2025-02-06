b6b61976fcf41f90aa59e829a994e0f35eefdd68
mulonemartin <mulone.martin@gmail.com>
2025-02-06 13:41:07 -0300

    fix: fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: './',
     envPrefix: "REACT_APP_",
     plugins: [
         react(),
