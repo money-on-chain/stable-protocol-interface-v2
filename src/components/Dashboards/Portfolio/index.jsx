188a4b4709c4f01fe69579f102337136ec810392
mulonemartin <mulone.martin@gmail.com>
2025-03-19 09:52:09 -0300

    fix: compile issues

--- src/components/Dashboards/Portfolio/index.jsx
+++ src/components/Dashboards/Portfolio/index.jsx
@@ -4,7 +4,6 @@ import BigNumber from "bignumber.js";
 import { useProjectTranslation } from "../../../helpers/translations";
 import { AuthenticateContext } from "../../../context/Auth";
 import settings from "../../../settings/settings.json";
-import BigNumber from "bignumber.js";
 import { fromContractPrecisionDecimals } from "../../../helpers/Formats";
 import { PrecisionNumbers } from "../../PrecisionNumbers";
 import { hasNonUSDPeggedTokens } from "../../../helpers/currencies";
